17ab1e8d6c007cbdd17bc62690c370c1c8e8131a
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-05-27 22:10:43 +0100

    More countries added

--- src/app.js
+++ src/app.js
@@ -4,15 +4,28 @@ const addWhisky = require('./helpers/add_whisky.js');
 // When DOM has loaded everything, start waiting for buttons to be pressed:
 document.addEventListener('DOMContentLoaded', () => {
 
-  // When Clear button is pressed, empty the list:
+  // If there are no whiskies in the list, render a message acknowedging that:
+  const naeWhisky = () => {
+    if (document.querySelector('.list-container>li') === null) {
+    const naeWhiskies = document.createElement('p');
+    naeWhiskies.textContent = 'Nae whisky? Whit is this, the High Middle Ages?';
+    document.querySelector('.list-container').appendChild(naeWhiskies);
+    }
+  }
+
+  naeWhisky();
+
+  // When "Clear list" is clicked, empty list and restore "nae whisky" message:
   document.querySelector('#delete-all').addEventListener('click', () => {
     const list = document.querySelector('.list-container');
     while (list.lastChild) {
       list.removeChild(list.lastChild);
     }
+    naeWhisky();
   });
 
-  // When Submit button is pressed, add whisky to the list and clear the form:
+  // When "Submit" button is pressed, remove the "nae whisky" message, add
+  // the submitted whisky to the list, and clear the form:
   document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
     const whisky = new Whisky(
@@ -21,6 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
       event.target['type'].value,
       event.target['country'].value
     );
+    const list = document.querySelector('.list-container');
+    const naeWhiskies = list.querySelector('p');
+    if (naeWhiskies) {
+      list.removeChild(naeWhiskies);
+    }
     addWhisky(whisky);
     event.target.reset();
   });
